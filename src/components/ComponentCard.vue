6f04f10dca893562e512d45a263561b50ee228e5
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-12-22 13:35:06 +0100

    add: aggiunte card dentro a file componentmain

---
+++ src/components/ComponentCard.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    card: "card",
+
+    props: {
+        cards: Array
+    }
+}
+
+</script>
+
+<template>
+    <div v-for="(card,i) in cards"   :key="i" class="card-iesima">
+        <img :src="card.thumb" alt="">
+        <div >
+            {{ card.series }}
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/general.scss' as *;
+@use '../style/partials/variable' as *;
+
+.card-iesima{
+    width: calc(100% / 6 - 10px);
+    margin: 0px 5px;
+    img{
+        width: 100%;
+    }
+}
+
+</style>
